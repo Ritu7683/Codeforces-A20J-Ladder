26f55699f9cdbe45c2bff9a4c1149645bfdd7251
Ritu Panchal <51368867+Ritu7683@users.noreply.github.com>
2021-09-21 10:29:09 +0530

    Add files via upload

---
+++ Codeforces Rating < 1300/A_Team.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long int
+using namespace std;
+
+int main() {
+int n;
+cin>>n;
+int a,b,c;
+int count=0;
+for(int i=0;i<n;i++){
+    cin>>a>>b>>c;
+    if((a+b+c)>=2){
+        count++;
+    }
+}
+cout<<count<<endl;
+return 0;
+}
