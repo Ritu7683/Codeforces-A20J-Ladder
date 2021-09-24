5746f9e8553bbc2db973893f05a2247f6900e526
Ritu Panchal <51368867+Ritu7683@users.noreply.github.com>
2021-09-24 19:40:59 +0530

    Add files via upload

---
+++ Codeforces Rating < 1300/A_Dima_and_Friends.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+int n;
+cin>>n;
+int arr[n];
+int sum=0;
+for(int i=0; i<n; i++){
+    cin>>arr[i];
+    sum+=arr[i];
+}
+int ans=0;
+for(int i=1;i<=5;i++){
+    if((sum+i)%(n+1)!=1){
+        ans++;
+    }
+}
+cout<<ans<<endl;
+return 0;
+}
